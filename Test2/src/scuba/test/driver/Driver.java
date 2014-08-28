4de81a9b57ce5cdf10c47d8834ef738d89966021
Yu Feng <fengyu8299@gmail.com>
2014-08-27 21:21:28 -0500

    update driver

--- Test2/src/scuba/test/driver/Driver.java
+++ Test2/src/scuba/test/driver/Driver.java
@@ -7,6 +7,8 @@ public class Driver {
 		scuba.test.testcase.virtualcall.Test.test();
 		// testing conjunction and disjunction of cst and container
 		scuba.test.testcase.virtualcall.Test.test();
+		// testing container
+		scuba.test.testcase.container.Test.testContainer();
 		// test intra-proc
 		scuba.test.testcase.intra.Test.test();
 	}
