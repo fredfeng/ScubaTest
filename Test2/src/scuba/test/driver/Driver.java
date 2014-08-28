d7f42f99033c0cf5e905c724e114f2dbda7fc181
Yu Feng <fengyu8299@gmail.com>
2014-08-28 18:10:33 -0500

    test case for design pattern

--- Test2/src/scuba/test/driver/Driver.java
+++ Test2/src/scuba/test/driver/Driver.java
@@ -9,6 +9,8 @@ public class Driver {
 		scuba.test.testcase.virtualcall.Test.test();
 		// testing container
 		scuba.test.testcase.container.Test.testContainer();
+		// testing design patterns
+		scuba.test.testcase.designpattern.Test.test();
 		// test intra-proc
 		scuba.test.testcase.intra.Test.test();
 	}
---
+++ Test2/src/scuba/test/testcase/designpattern/DogFactory.java
@@ -0,0 +1,12 @@
+package scuba.test.testcase.designpattern;
+
+public class DogFactory {
+	public static Dog getDog(String criteria) {
+		if (criteria.equals("small"))
+			return new Poodle();
+		else if (criteria.equals("big"))
+			return new Rottweiler();
+
+		return null;
+	}
+}
