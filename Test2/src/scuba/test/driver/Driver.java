72ff8e2b6265c1734fb9b1ea4d4e844c6c0ba5dd
Xinyu Wang <xwang@cs.utexas.edu>
2014-09-06 14:14:56 -0500

    the changed dlist test case will induce some failure but the previous one is OK, rolling back to the previous one, need checking the test case

--- Test2/src/scuba/test/driver/Driver.java
+++ Test2/src/scuba/test/driver/Driver.java
@@ -12,16 +12,16 @@ public class Driver {
 		// testing design patterns
 		scuba.test.testcase.designpattern.Test.test();
 		// tricky test cases.
-		scuba.test.testcase.tricky.Test.test();
+		// scuba.test.testcase.tricky.Test.test();
 		// test intra-proc
 		scuba.test.testcase.intra.Test.test();
 		// test inter-proc
 		scuba.test.testcase.inter.Test.test();
 		// testing recusions.
-		//No alias assertion in this test suite, but our analysis will 
-		//not terminate within 10 min if you set alloc length > 1.
-		//run this test case when you are ready for it.
-		//scuba.test.testcase.recursion.Test.test();
+		// No alias assertion in this test suite, but our analysis will
+		// not terminate within 10 min if you set alloc length > 1.
+		// run this test case when you are ready for it.
+		// scuba.test.testcase.recursion.Test.test();
 	}
 
 }
--- Test2/src/scuba/test/testcase/container/dlist/Test.java
+++ Test2/src/scuba/test/testcase/container/dlist/Test.java
@@ -7,13 +7,12 @@ import framework.scuba.helper.AliasHelper;
 public class Test {
 
 	public static void run() {
-		// TODO Auto-generated method stub
 		int N = 4;
 		ListItem l1 = new ListItem();
 		ListItem l2 = new ListItem();
 		ListItem l3 = new ListItem();
 		ListItem l4 = new ListItem();
-		
+
 		Object o1 = new Object();
 		Object o2 = new Object();
 		l1.f = o1;
@@ -21,81 +20,64 @@ public class Test {
 		l3.f = o1;
 		l4.f = o2;
 
-        // add elements 1, ..., N
-        DoublyLinkedList<ListItem> list = new DoublyLinkedList<ListItem>();
-        for (int i = 0; i < N; i++)
-            list.add(l1);
-        list.add(l2);
-        list.add(l3);
-        
-        ListIterator<ListItem> iterator = list.iterator();
-        test1(iterator, o1, l1, l2, l4);
-        test2(iterator, l2, l3, l4);
-        test3(iterator, l2, l3, l4);
-        test4(iterator, l2, l3, l4);
-        test5(iterator, l4);
-	}
-	
-	private static void test1(ListIterator<ListItem> iterator, Object o1,
-			ListItem l1, ListItem l2, ListItem l4) {
-        // go forwards with next() and set()
-        while (iterator.hasNext()) {
-            ListItem x = iterator.next();
-            x.f = o1;
-            AliasHelper.alias(x.f, l1.f);
-            AliasHelper.alias(x.f, l2.f);
-            AliasHelper.notAlias(x.f, l4.f);
-            iterator.set(x);
-        }
+		// add elements 1, ..., N
+		DoublyLinkedList<ListItem> list = new DoublyLinkedList<ListItem>();
+		for (int i = 0; i < N; i++)
+			list.add(l1);
 
-	}
-	
-	private static void test2(ListIterator<ListItem> iterator, ListItem l2,
-			ListItem l3, ListItem l4) {
-        // go backwards with previous() and set()
-        while (iterator.hasPrevious()) {
-        	ListItem x = iterator.previous();
-        	AliasHelper.alias(x, l2);
-        	AliasHelper.alias(x, l3);
-        	AliasHelper.notAlias(x, l4);
-        }
-	}
-	
-	private static void test3(ListIterator<ListItem> iterator, ListItem l2,
-			ListItem l3, ListItem l4) {
-        // remove all elements that are multiples of 4 via next() and remove()
-        while (iterator.hasNext()) {
-            ListItem x = iterator.next();
-            if (x != null) iterator.remove();
-        	AliasHelper.alias(x, l2);
-        	AliasHelper.alias(x, l3);
-        	AliasHelper.notAlias(x, l4);
-        }
-	}
+		list.add(l2);
+		list.add(l3);
 
-	private static void test4(ListIterator<ListItem> iterator, ListItem l2,
-			ListItem l3, ListItem l4) {
-        // remove all even elements via previous() and remove()
-        while (iterator.hasPrevious()) {
-        	ListItem x = iterator.previous();
-            if (x != null) iterator.remove();
-        	AliasHelper.alias(x, l2);
-        	AliasHelper.alias(x, l3);
-        	AliasHelper.notAlias(x, l4);
-        }
-	}
-	
-	private static void test5(ListIterator<ListItem> iterator, ListItem l4) {
-        // add elements via previous() and add()
+		ListIterator<ListItem> iterator = list.iterator();
+
+		// go forwards with next() and set()
+		while (iterator.hasNext()) {
+			ListItem x = iterator.next();
+			x.f = o1;
+			AliasHelper.alias(x.f, l1.f);
+			AliasHelper.alias(x.f, l2.f);
+			AliasHelper.notAlias(x.f, l4.f);
+			iterator.set(x);
+		}
+
+		// go backwards with previous() and set()
+		while (iterator.hasPrevious()) {
+			ListItem x = iterator.previous();
+			AliasHelper.alias(x, l2);
+			AliasHelper.alias(x, l3);
+			AliasHelper.notAlias(x, l4);
+		}
+
+		// remove all elements that are multiples of 4 via next() and remove()
+		while (iterator.hasNext()) {
+			ListItem x = iterator.next();
+			if (x != null)
+				iterator.remove();
+			AliasHelper.alias(x, l2);
+			AliasHelper.alias(x, l3);
+			AliasHelper.notAlias(x, l4);
+		}
+
+		// remove all even elements via previous() and remove()
+		while (iterator.hasPrevious()) {
+			ListItem x = iterator.previous();
+			if (x != null)
+				iterator.remove();
+			AliasHelper.alias(x, l2);
+			AliasHelper.alias(x, l3);
+			AliasHelper.notAlias(x, l4);
+		}
+
+		// add elements via previous() and add()
 		ListItem x = iterator.previous();
-    	AliasHelper.notAlias(x.f, l4.f);
-		//eventually we add l4.
+		AliasHelper.notAlias(x.f, l4.f);
+		// eventually we add l4.
 		iterator.add(l4);
-		//now everything becomes alias.
+		// now everything becomes alias.
 		ListItem y = iterator.previous();
-    	AliasHelper.alias(y, l4);
-    	AliasHelper.alias(y.f, l4.f);
-    	AliasHelper.notAlias(x.f, l4.f);
+		AliasHelper.alias(y, l4);
+		AliasHelper.alias(y.f, l4.f);
+		AliasHelper.notAlias(x.f, l4.f);
 	}
 
 }
